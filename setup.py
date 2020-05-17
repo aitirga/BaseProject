493e5f808899d245d05f250e471acf47577bc519
Aitor IG <aitirga@gmail.com>
2020-05-17 22:35:48 +0200

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='baseproject',
+    version='',
+    packages=['baseproject', 'baseproject.model', 'baseproject.tests', 'baseproject.tests.model',
+              'baseproject.tests.utils', 'baseproject.tests.source', 'baseproject.tests.preprocess',
+              'baseproject.utils', 'baseproject.source', 'baseproject.preprocess'],
+    url='',
+    license='',
+    author='Aitirga',
+    author_email='aitirga@gmail.com',
+    description=''
+)
