3a34d1737341d3c59614e6d705a11e01bff17c20
Aitor Iraola <aitirga@gmail.com>
2020-05-19 11:46:04 +0200

    feat: implement basic logging

--- baseproject/config.py
+++ baseproject/config.py
@@ -1,8 +1,23 @@
 import yaml
 from box import Box
 import os
+from dotenv import load_dotenv
+import logging.config
+load_dotenv()
 
-
+env = os.environ["ENVIRONMENT"]
 config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'configs/config.yaml'))
 with open(config_path) as yml_file:
-    cfg = Box(yaml.safe_load(yml_file))
+    full_cfg = Box(yaml.safe_load(yml_file), default_box=True)
+    full_cfg.base.path.root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+cfg = Box({**full_cfg["base"], **full_cfg[env]}, default_box=True, default_box_attr=None)
+
+os.makedirs(cfg.path.logs, exist_ok=True)
+log_config_path = os.path.join(cfg.path.root, cfg.path.log_config)
+if os.path.exists(log_config_path):
+    with open(log_config_path, "r") as log_config_file:
+        log_config = yaml.safe_load(log_config_file)
+    logging.config.dictConfig(log_config)
+else:
+    raise FileNotFoundError(f"Log yaml configuration file not found on {log_config_path}")
---
+++ baseproject/model/samplers/BaseSampler.py
@@ -0,0 +1,6 @@
+"""This module contains a base case of a data sampler"""
+
+
+class BaseSampler:
+    def __init__(self, *args, **kwargs):
+        pass
