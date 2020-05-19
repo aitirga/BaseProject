b7c1967e3bedcf8c352df7688fdc1f87c7364d99
Aitor Iraola <aitirga@gmail.com>
2020-05-19 12:01:56 +0200

    feat: implement basic unittest

---
+++ baseproject/tests/test_config.py
@@ -0,0 +1,18 @@
+import unittest
+from baseproject.config import cfg
+import yaml
+import os
+import logging
+logging.disable()
+
+
+class ConfigCase(unittest.TestCase):
+    def test_config(self):
+        config_path = os.path.abspath(os.path.join(__file__, "..", "..", "..", "configs/config.yaml"))
+        with open(config_path, "r") as test_file:
+            test_dict = yaml.safe_load(test_file)
+        self.assertEqual(cfg.path.logs, test_dict["base"]["path"]["logs"])
+
+
+if __name__ == "__main__":
+    unittest.main()
